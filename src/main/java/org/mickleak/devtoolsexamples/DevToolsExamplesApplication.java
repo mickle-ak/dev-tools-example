deccc8e1d3347085b127927e86d06c9d531622a8
kiselev <Mikhail.Kiselev@adesso.de>
2026-03-16 17:56:58 +0100

    create project

---
+++ src/main/java/org/mickleak/devtoolsexamples/DevToolsExamplesApplication.java
@@ -0,0 +1,14 @@
+package org.mickleak.devtoolsexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DevToolsExamplesApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( DevToolsExamplesApplication.class, args );
+	}
+
+}
---
+++ src/test/java/org/mickleak/devtoolsexamples/DevToolsExamplesApplicationTests.java
@@ -0,0 +1,14 @@
+package org.mickleak.devtoolsexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class DevToolsExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
